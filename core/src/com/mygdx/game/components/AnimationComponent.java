b0d3ebb296bc6609e3ae29e007c16b4905074f2d
monafrank <mona.frank@rwth-aachen.de>
2020-04-03 18:20:54 +0200

    Merge branch 'master' of /Users/monafrank/Desktop/SoftwareArchitecture/Project 1/chicken-parkour with conflicts.

---
+++ core/src/com/mygdx/game/components/AnimationComponent.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.components;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class AnimationComponent {
+
+    public void jump(){}
+
+    public void walk(){}
+
+    //only used with SpeedPowerUp
+    public void run(){}
+
+    public void slide(){}
+
+    public void idle(){}
+
+
+}
