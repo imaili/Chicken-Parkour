4906f80175dde1100778d134fa74dbfda9fc2c73
Jose Ruiz <jose.ruiz.alarcon@gmail.com>
2020-04-20 15:13:21 +0200

    GameOverScreen deleted

--- core/src/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -107,29 +107,6 @@ public abstract class MenuScreen extends BaseScreen implements Menu {
         MainGame.getSingleton().getMusicManager().stop_music();
     }
 
-    public static Skin createBasicSkin() {
-        // Create a font
-        BitmapFont font = new BitmapFont();
-        Skin skin = new Skin();
-        skin.add("default", font);
-
-        // Create a texture
-        Pixmap pixmap = new Pixmap(Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("background", new Texture(pixmap));
-
-        // Create a button style
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
-        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.checked = skin.newDrawable("background", Color.GRAY);
-        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
-        return skin;
-    }
-
     // Override from BaseScreen
 
     public void draw() {
